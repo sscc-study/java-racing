21e6d2613635559342b0a259fce5420768ddbf02
Yerin Song <angelrin7@naver.com>
2022-07-14 21:52:18 +0900

    두 파일 모두 과제에 맞는 코드 작성 완료했습니다.

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class StringTest {
+    @Test
+    public void split(){
+        String[] first = "1,2".split(",");
+        String[] second = "1,".split(",");
+
+        assertThat(first).contains("1","2");
+        assertThat(second).containsExactly("1");
+    }
+    @Test
+    public void subString(){
+        String basic="(1,2)";
+        String result=basic.substring(1,basic.length()-1);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+    @Test
+    @DisplayName("abc 문자열이 주어졌을 때 특정 위치의 문자 가져오는 테스트")
+    public void charAtTest() {
+        String str="abc";
+
+        assertThatThrownBy(()-> str.charAt(3))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessage("String index out of range: 3");
+    }
 }
