82a9dcb7aa42a7953765c07b9e9d6d971d230c47
천지홍 <clara5091@naver.com>
2022-07-18 07:15:25 +0900

    reformat

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,82 +6,82 @@ public class Calculator {
     private int num2;
 
     // 덧셈
-    private int addition(){
-        return num1+num2;
+    private int addition() {
+        return num1 + num2;
     }
 
     // 뺄셈
-    private int subtraction(){
-        return num1-num2;
+    private int subtraction() {
+        return num1 - num2;
     }
 
     // 나눗셈
-    private int division(){
-        return num1/num2;
+    private int division() {
+        return num1 / num2;
     }
 
     // 곱하기
-    private int multiplication(){
-        return num1*num2;
+    private int multiplication() {
+        return num1 * num2;
     }
 
     // 문장 입력 후 공백단위로 분리
-    private void getStrings(String input){
+    private void getStrings(String input) {
         inputs = input.split(" ");
     }
 
     // 사칙연산 기호 아닌지 검사 -> 맞을 시 계산된 값 리턴
-    private int checkOperation(String operation){
-        if(operation.equals("+"))
+    private int checkOperation(String operation) {
+        if (operation.equals("+"))
             return addition();
-        if(operation.equals("-"))
+        if (operation.equals("-"))
             return subtraction();
-        if(operation.equals("*"))
+        if (operation.equals("*"))
             return multiplication();
-        if(operation.equals("/"))
+        if (operation.equals("/"))
             return division();
         throw new IllegalArgumentException("올바르지 않은 연산자입니다.");
     }
 
     // 공백만 입력되었는 지 확인
-    private void checkBlank(String input){
-        if(input.equals(""))
+    private void checkBlank(String input) {
+        if (input.equals(""))
             throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
     }
 
     // 입력값이 없는 지 확인
-    private void checkEmpty(){
-        if(inputs.length==0)
+    private void checkEmpty() {
+        if (inputs.length == 0)
             throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
     }
 
     // 입력된 값이 null인 지 검사
-    private void checkNull(String input){
-        if(input==null)
+    private void checkNull(String input) {
+        if (input == null)
             throw new IllegalArgumentException("null이 입력되었습니다.");
     }
 
     // 문자 검사
-    private void calculate(){
+    private void calculate() {
         num1 = Integer.parseInt(inputs[0]);
-        for(int i=1; i< inputs.length; i+=2){
-            num2 = Integer.parseInt(inputs[i+1]);
+        for (int i = 1; i < inputs.length; i += 2) {
+            num2 = Integer.parseInt(inputs[i + 1]);
             num1 = checkOperation(inputs[i]);
         }
     }
 
     // 결과값 출력
-    private void printResult(){
+    private void printResult() {
         System.out.println(num1);
     }
 
     // 결과값 리턴
-    public int getResult(){
+    public int getResult() {
         return num1;
     }
 
     // 최종 실행
-    public int run(String input){
+    public int run(String input) {
         checkNull(input);
         checkBlank(input);
         getStrings(input);
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,14 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
-    void 더하기_테스트(){
+    void 더하기_테스트() {
         // given
         String test = "1 + 2 + 3";
         int expected = 6;
@@ -21,7 +21,7 @@ class CalculatorTest {
     }
 
     @Test
-    void 뺄셈_테스트(){
+    void 뺄셈_테스트() {
         // given
         String test = "7 - 2 - 3";
         int expected = 2;
@@ -34,7 +34,7 @@ class CalculatorTest {
     }
 
     @Test
-    void 곱하기_테스트(){
+    void 곱하기_테스트() {
         // given
         String test = "7 * 2 * 3";
         int expected = 42;
@@ -47,7 +47,7 @@ class CalculatorTest {
     }
 
     @Test
-    void 나누기_테스트(){
+    void 나누기_테스트() {
         // given
         String test = "20 / 2 / 5";
         int expected = 2;
@@ -60,40 +60,40 @@ class CalculatorTest {
     }
 
     @Test
-    void 공백문자_테스트(){
+    void 공백문자_테스트() {
         // given
-        String test="    ";
+        String test = "    ";
 
         // then
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             calculator.run(test);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 입력값이_없을때_테스트(){
+    void 입력값이_없을때_테스트() {
         // given
-        String test="";
+        String test = "";
 
         // then
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             calculator.run(test);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 입력값이_null일때_테스트(){
+    void 입력값이_null일때_테스트() {
         // given
-        String test=null;
+        String test = null;
 
         // then
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             calculator.run(test);
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 순서대로_계산되는_지_확인(){
+    void 순서대로_계산되는_지_확인() {
         // given
         String test = "2 + 3 * 4 / 2";
         int expected = 10;
@@ -106,12 +106,12 @@ class CalculatorTest {
     }
 
     @Test
-    void 사칙연산기호_확인_테스트(){
+    void 사칙연산기호_확인_테스트() {
         // given
-        String test="1 + 2 % 5";
+        String test = "1 + 2 % 5";
 
         // then
-        assertThatThrownBy(()->{
+        assertThatThrownBy(() -> {
             calculator.run(test);
         }).isInstanceOf(IllegalArgumentException.class);
     }
