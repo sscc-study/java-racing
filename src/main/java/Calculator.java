45cd22069103543ed270f2f96e708d07caf28fdd
천지홍 <clara5091@naver.com>
2022-07-19 04:59:05 +0900

    코드 수정

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,24 +11,15 @@ public class Calculator {
     // 문장 입력 후 공백단위로 분리
     private void getStrings(String input) {
         inputs = input.split(" ");
+        if (inputs.length == 0){
+            throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
+        }
     }
 
     // 공백만 입력되었는 지 확인
     private void checkBlank(String input) {
-        if (input.equals(""))
+        if (input == null||input.equals(""))
             throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
-    }
-
-    // 입력값이 없는 지 확인
-    private void checkEmpty() {
-        if (inputs.length == 0)
-            throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
-    }
-
-    // 입력된 값이 null인 지 검사
-    private void checkNull(String input) {
-        if (input == null)
-            throw new IllegalArgumentException("null이 입력되었습니다.");
     }
 
     // 계산
@@ -42,10 +33,8 @@ public class Calculator {
 
     // 최종 실행
     public int run() {
-        checkNull(input);
         checkBlank(input);
         getStrings(input);
-        checkEmpty();
         calculate();
         return num1;
     }
